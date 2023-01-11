5f49db71a591e64d900662c7f7877b6edeb0f82e
Midori <midorili998@gmail.com>
2023-01-10 20:20:38 -0800

    "Complete bare_minimum/callbackReview.js exercises"

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,27 @@ var fs = require('fs');
 var request = require('needle');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, cb) {
+  fs.readFile(filePath, 'utf8', function(err, content) {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, content.slice(0, 22));
+    }
+  });
   // TODO
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, cb) {
   // TODO
+  request.get(url, function(err, response) {
+    if (!err) {
+      cb(null, response.statusCode);
+    } else {
+      cb(err, null);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
